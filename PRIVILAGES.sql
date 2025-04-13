e76021b0694da0b211c7aedde47cab3983db7440
Bonface Mamboleo <mamboleobonface001@gmail.com>
2025-04-13 22:48:52 +0300

    Update PRIVILAGES.sql

--- PRIVILAGES.sql
+++ PRIVILAGES.sql
@@ -5,8 +5,8 @@ CREATE USER 'Sylvester_user'@'%' IDENTIFIED BY 'SylvesterPass123';
 CREATE USER 'George_user'@'%' IDENTIFIED BY 'GeorgePass456';
 CREATE USER 'read_user'@'%' IDENTIFIED BY 'ReadOnly123';
 GRANT ALL PRIVILEGES ON bookstore_db.* TO 'Bonface_Mamboleo'@'localhost';
-GRANT SELECT, INSERT, UPDATE ON bookstore_db.* TO 'Sylvester_user'@'%';
-GRANT SELECT, INSERT, UPDATE ON bookstore_db.* TO 'George_user'@'%';
+GRANT CREATE, DELETE, SELECT, INSERT, UPDATE ON bookstore_db.* TO 'Sylvester_user'@'%';
+GRANT CREATE, DELETE, SELECT, INSERT, UPDATE ON bookstore_db.* TO 'George_user'@'%';
 GRANT SELECT ON bookstore_db.* TO 'read_user'@'%';
 
 FLUSH PRIVILEGES;
